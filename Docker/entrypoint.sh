746ecbb1676a734f8c1a12923f318e0e8c0ce3ba
Nick Chan <nick.chan@utexas.edu>
2024-04-04 19:17:42 -0500

    run client on start

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -4,7 +4,8 @@ set -e
 
 source /opt/ros/iron/setup.bash
 source robofleet_client_ws/install/setup.bash
-ros2 run robofleet_client client robofleet_client_ws/src/robofleet_client/cfg/config.yaml
 
+ros2 run robofleet_client client robofleet_client_ws/src/robofleet_client/cfg/config.yaml
 exec "$@"
+
 # exec /bin/bash
